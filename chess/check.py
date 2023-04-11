ca972e2930ccb80836ca2e6ac873028fe17e93ce
Margia Rounok <mr879@cornell.edu>
2023-04-11 15:22:00 -0400

    refactored check.py need to convert pos to string

--- chess/check.py
+++ chess/check.py
@@ -4,7 +4,9 @@
 # kings is unsafe or not
 board = []
 
-def get_piece(piece_id, color): 
+#TODO: convert to our game code and then slowly integrate everything
+#TODO: change x to letters
+def get_piece(piece_id, is_white): 
 	return [] #TODO: returns list of all pieces of this color 
 
 def checkBoard(is_white_to_play, king_pos, curr_color: bool):
@@ -12,31 +14,46 @@ def checkBoard(is_white_to_play, king_pos, curr_color: bool):
   j = king_pos[1]
 
   # Check for Knight
-  if checkKnight('N', i, j):
+  if checkKnight(is_white, i, j):
     return True
 
   # Check for Pawn
-  if checkPawn('P', i, j):
+  if checkPawn(is_white, i, j):
     return True
 
   # Check for Rook
-  if lookForr('R', i, j):
+  if checkRook(is_white, i, j):
     return 1
 
   # Check for Bishop
-  if lookForb('B', i, j):
+  if checkBishop(is_white, i, j):
     return 1
 
   # Check for Queen
-  if lookForq('Q', i, j):
+  if checkQueen(is_white, i, j):
     return 1
 
   # Check for King
-  if lookFork('K', i, j):
+  if checkKing(is_white, i, j):
     return 1
 
+def checkKnight(board, c, i, j):
+    # All possible moves of the knight
+    x = [ 2, 2, -2, -2, 1, 1, -1, -1 ]
+    y = [ 1, -1, 1, -1, 2, -2, 2, -2 ]
+ 
+    for k in range(8):
+        # Incrementing index values
+        m = i + x[k]
+        n = j + y[k]
+        pos = str(m) + str(n)
+        # Checking boundary conditions
+        # and character match
+        if inBounds(m, n) and self.board.get(pos)._is_white == c:
+            return True
+    return False
 
-def checkKnight(c, i, j, curr_color):
+def checkKing(curr_color, i, j):
 	# Store all possible moves of the king
 	x = [ -1, -1, -1, 0, 0, 1, 1, 1 ]
 	y = [ -1, 0, 1, -1, 1, -1, 0, 1 ]
@@ -53,100 +70,83 @@ def checkKnight(c, i, j, curr_color):
 	return False
 
 # Function to check if Queen can attack the King
-def lookForq(board, c, i, j):
+def checkQueen(c, i, j):
 
 	# Queen's moves are a combination
 	# of both the Bishop and the Rook
-	if lookForb(board, c, i, j) or lookForr(board, c, i, j):
+	if checkBishop(c, i, j) or checkRook(c, i, j):
 		return True
 	return False
 
 # Function to check if bishop can attack the king
-def lookForb(board, c, i, j):
+def checkBishop(board, c, i, j):
 	# Check the lower right diagonal
 	k = 0
 	while inBounds(i + ++k, j + k):
-		if board[i + k][j + k] == c:
+		if self.board.get(str(i+k) + str(j+k))._is_white == c:
 			return True
-		if board[i + k][j + k] != '-':
+		if self.board.get(str(i+k) + str(j+k)) != None:
 			break
 
 	# Check the lower left diagonal
 	k = 0
 	while inBounds(i + ++k, j - k):
-		if board[i + k][j - k] == c:
+		if self.board.get(str(i+k) + str(j-k))._is_white == c:
 			return True
-		if board[i + k][j - k] != '-':
+		if self.board.get(str(i+k) + str(j-k)) != None:
 			break
 
 	# Check the upper right diagonal
 	k = 0
 	while inBounds(i - ++k, j + k):
-		if board[i - k][j + k] == c:
+		if self.board.get(str(i-k) + str(j+k))._is_white == c:
 			return True
-		if board[i - k][j + k] != '-':
+		if self.board.get(str(i-k) + str(j+k))._is_white != None:
 			break
 
 	# Check the upper left diagonal
 	k = 0
 	while inBounds(i - ++k, j - k):
-		if board[i - k][j - k] == c:
+		if self.board.get(str(i-k) + str(j-k))._is_white == c:
 			return True
-		if board[i - k][j - k] != '-':
+		if self.board.get(str(i-k) + str(j-k)) != None:
 			break
 
 	return False
 
 # Check if
-def lookForr(board, c, i, j):
+def checkRook(c, i, j):
 	# Check downwards
 	k = 0
 	while inBounds(i + ++k, j):
-		if board[i + k][j] == c:
+		if self.board.get(str(i+k) + str(j))._is_white == c:
 			return True
-		if board[i + k][j] != '-':
+		if self.board.get(str(i+k) + str(j)) != None:
 			break
 
 	# Check upwards
 	k = 0
 	while inBounds(i + --k, j):
-		if board[i + k][j] == c:
+		if self.board.get(str(i+k) + str(j))._is_white == c:
 			return True
-		if board[i + k][j] != '-':
+		if self.board.get(str(i+k) + str(j)) != None:
 			break
 
 	# Check right
 	k = 0
 	while inBounds(i, j + ++k):
-		if board[i][j + k] == c:
+		if self.board.get(str(i) + str(j+k))._is_white == c:
 			return True
-		if board[i][j + k] != '-':
+		if self.board.get(str(i) + str(j+k)) != None:
 			break
 
 	# Check left
 	k = 0
 	while inBounds(i, j + --k):
-		if board[i][j + k] == c:
+		if self.board.get(str(i) + str(j+k))._is_white:
 			return True
-		if board[i][j + k] != '-':
+		if self.board.get(str(i) + str(j+k)) != None:
 			break
-	return False
-
-# Check if the knight can attack the king
-def lookForn(board, c, i, j):
-	# All possible moves of the knight
-	x = [ 2, 2, -2, -2, 1, 1, -1, -1 ]
-	y = [ 1, -1, 1, -1, 2, -2, 2, -2 ]
-
-	for k in range(8):
-		# Incrementing index values
-		m = i + x[k]
-		n = j + y[k]
-
-		# Checking boundary conditions
-		# and character match
-		if inBounds(m, n) and board[m][n] == c:
-			return True
 	return False
 
 # Function to check if pawn can attack the king
