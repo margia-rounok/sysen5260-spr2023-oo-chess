638938bb8de26ccc5e65f2977d65ea3b55b9f1e7
mike-eaton <104577440+mike-eaton@users.noreply.github.com>
2023-04-16 18:23:17 -0400

    renamed main file to main_old to preserve other branche until merged

--- chess/__main__.py
+++ chess/__main__.py
@@ -1,44 +1,20 @@
-import chess.model as model
-import chess.view as view
+import chess.model_2 as model
+import chess.view_2 as view
 
+import chess.rules as rules
 
-rules = model.Rules()
-
-#TODO move none type 
+rules = rules.Rules()
 game = model.Game()
 game.set_up_pieces()
+
 while not game.game_over:
     print("")
     print(view.board_to_text(game.board))
-    move = ""
-    while True: 
-        prompt = "White to play: " if game.white_to_play else "Black to play: "
-        move = input(prompt)
-        game.move_history_append(move)
-        #game.piece_list_append(move)
-        if move == 'backup':
-             game.do_backup(move)
-             break
-        # elif game.is_checkmate(): 
-        #      print("Your king has been checkmated. Game over.")
-        #      break
-        elif game.check_input(move) is None or not game.check_moved_op(move): #valid move
-            # and not game.check_move_exist(move)
-            # print("here")
-            print("ERROR: Improper source move. Please try again.")
-        elif not game.check_no_piece_override(move):
-            print("ERROR: Improper destination move. Please try again.")
+    prompt = "White to play:" if game.white_to_play else "Black to play:"
+    move = input(prompt)
+    legal_move, message, captured_piece_location = rules.validate_move(move, game)
 
-        else:
-            source_type = game.get_source_type(move)
-            if game.check_king(move):
-                print("ERROR: Your king is in check with this move. Please try again") 
-            elif not game.check_no_piece_override(move) or not game.check_piece(move, source_type):
-                    print("ERROR: Improper destination move for pawn. Please try again.")
-            else:
-                print("in else")
-                break
-        
-    # prompt = "White to play:" if game.white_to_play else "Black to play:"
-    # move = input(prompt)
-    # game.accept_move(move)
+    if legal_move:
+        game.accept_move(move,captured_piece_location)
+    else:
+        print(message)
