7ced29e9ed06354127251cf67f13d09c714d5eb2
Margia Rounok <mr879@cornell.edu>
2023-04-16 19:12:10 -0400

    moved test file

--- tests/test_moves.py
+++ tests/test_moves.py
@@ -1,7 +1,6 @@
-
-import pytest
+# import pytest
 from chess import rules
-from chess.model import Game, Board, Piece, Pawn, Rook, Knight, Bishop, Queen, King
+from chess.model_2 import Game, Board, Piece, Pawn, Rook, Knight, Bishop, Queen, King
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8']
