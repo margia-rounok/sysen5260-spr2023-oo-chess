7c364146dc3a39a4bbd547eb21cef958ed5b0df0
Margia Rounok <mr879@cornell.edu>
2023-04-17 15:43:39 -0400

    view_2 has 100% test coverage

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM python:3.10
 RUN pip install pandas pytest
+RUN pip install coverage
--- tests/test_view.py
+++ tests/test_view.py
@@ -1,7 +1,22 @@
-import chess.view as view
+import chess.view_2 as view
 import chess.model as model
+from chess.model_2 import Game 
 
 def test_piece_to_char():
     assert '·' == view.piece_to_char(None)
     assert '♙' == view.piece_to_char(model.Pawn(is_white=True))
     assert '♟' == view.piece_to_char(model.Pawn(is_white=False))
+
+def test_init_board():
+    game = Game()
+    game.set_up_pieces()
+    board = game.get_board()
+    assert view.board_to_text(board)[0:18] == '  a b c d e f g h\n'
+    assert view.board_to_text(board)[18:37] == '8 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ \n'
+    assert view.board_to_text(board)[37:56] == '7 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ \n'
+    assert view.board_to_text(board)[56:75] == '6 · · · · · · · · \n'
+    assert view.board_to_text(board)[75:94] == '5 · · · · · · · · \n'
+    assert view.board_to_text(board)[94:113] == '4 · · · · · · · · \n'
+    assert view.board_to_text(board)[113:132] == '3 · · · · · · · · \n'
+    assert view.board_to_text(board)[132:151] == '2 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ \n'
+    assert view.board_to_text(board)[151:] == '1 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ \n'
